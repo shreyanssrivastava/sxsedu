b57499b201dd4b5cea26e480db65595dac1a2f65
Shreyans Srivastava <shreyansxsrivastava@gmail.com>
2026-05-31 13:11:15 +0530

    Add CORS headers to pincode API handler

--- api/pincode.js
+++ api/pincode.js
@@ -1,3 +1,5 @@
+import https from "https";
+
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
