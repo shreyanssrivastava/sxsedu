f0ecca9246d3516df9e4162c5b46936dbc3fe066
Shreyans Srivastava <shreyansxsrivastava@gmail.com>
2026-05-31 12:18:45 +0530

    Adding pincode api!
    - To get city & state on reg.html page.

---
+++ api/pincode.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  try {
+  
+    const { code } = JSON.parse(req.body);
+ 
+    const response = await fetch(`https://api.postalpincode.in/pincode/${code}`);
+    const data = await response.json();
+    const result = data[0].PostOffice[0];
+    
+    if (data[0].Status === "Success") {  
+      res.status(200).json({ city: result.District, state: result.State });
+    } else {
+        res.status(404).json({ error: "Invalid Pincode!" });
+    }
+
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+}
